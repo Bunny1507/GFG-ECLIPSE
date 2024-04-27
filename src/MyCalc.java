845c1a2dd9ba1a9a7fbd2ee0d8b8d6b7ff6c2660
Manikanta <sriramoju.manii1507@gmail.com>
2024-04-27 12:43:04 +0530

    Diff() Added

--- src/MyCalc.java
+++ src/MyCalc.java
@@ -6,10 +6,15 @@ public class MyCalc {
 	{
 		return a+b;
 	}
+	int diff(int a , int b)
+	{
+		return a-b;
+	}
+	
 	
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println("sum is:"+calc.sum(10,20));
-		
+		System.out.println("sum is:"+calc.diff(10,20));
 	}
 }
